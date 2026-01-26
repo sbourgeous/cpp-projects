73b52dc304ac3b936087f403f52644446555514d
sbourgeous <bou20009@byui.edu>
2026-01-26 14:24:22 -0700

    Finalized V.2 (Still need to comment code and attempt to improve appearance)

--- to_do_list/main.cpp
+++ to_do_list/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -27,6 +28,8 @@ void showMenu();
 void addTask(vector<Task>& tasks);
 void viewTasks(const vector<Task>& tasks);
 void completeTask(vector<Task>& tasks);
+void deleteTask(vector<Task>& tasks);
+void saveToFile(const vector<Task>& tasks);
 
 //-------- Main --------
 
@@ -34,7 +37,7 @@ int main() {
     vector<Task> tasks;
     int choice = 0;
 
-    while (choice != 4) {
+    while (choice != 6) {
         showMenu();
         cin >> choice;
 
@@ -48,6 +51,12 @@ int main() {
             completeTask(tasks);
         }
         else if (choice == 4) {
+            deleteTask(tasks);
+        }
+        else if (choice ==5) {
+            saveToFile(tasks);
+        }
+        else if (choice == 6) {
             cout << "Exiting program.\n";
         }
         else {
@@ -59,11 +68,16 @@ int main() {
 //-------- Function Definitions--------
 
 void showMenu() {
-    cout << "\n--- Menu ---\n";
+    cout << "\n============================\n";
+    cout << "          MAIN MENU         \n";
+    cout << "============================\n";
     cout << "1. Add Task\n";
-    cout << "2. View tasks\n";
+    cout << "2. View Tasks\n";
     cout << "3. Mark Task Complete\n";
-    cout << "4. Exit\n";
+    cout << "4. Delete Task\n";
+    cout << "5. Save To File\n";
+    cout << "6. Exit\n";
+    cout << "============================\n";
     cout << "Enter choice: ";
 }
 
@@ -98,7 +112,7 @@ void completeTask(vector<Task>& tasks) {
 
     if (tasks.empty()) return;
 
-    cout << "Enter task number to complete\n";
+    cout << "Enter task number to complete: \n";
     cin >> index;
 
     if (index < 1 || index > tasks.size()) {
@@ -108,4 +122,40 @@ void completeTask(vector<Task>& tasks) {
 
     tasks[index - 1].markComplete();
     cout << "Task marked complete.\n";
+}
+
+void deleteTask(vector<Task>& tasks) {
+    int index;
+    viewTasks(tasks);
+
+    if (tasks.empty()) return;
+
+    cout << "Enter task number to delete: \n";
+    cin >> index;
+
+    if (index < 1 || index > tasks.size()) {
+        cout << "Invalid task number.\n";
+        return;
+    }
+
+    tasks.erase(tasks.begin() + (index - 1));
+    cout << "Task deleted.\n";
+}
+
+void saveToFile(const vector<Task>& tasks) {
+    ofstream file("tasks.txt");
+
+    if (!file) {
+        cout << "Error opening file.\n";
+        return;
+    }
+
+    for (const auto& task : tasks) {
+        file << task.description << " | "
+             << (task.completed ? "Completed" : "Pending")
+             << "\n";
+    }
+
+    file.close();
+    cout << "Tasks saved to tasks.txt\n";
 }
